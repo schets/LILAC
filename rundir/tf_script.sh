8909d4d82fc243476de6a199a4eaf40588ba5e5b
Sam Schetterer <schets@uw.edu>
2014-02-12 11:10:27 -0800

    Hoperfully consistent nersc scripts

---
+++ rundir/tf_script.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+fout=$( printf 'lilac_output/run%04d.out' $TF_TASKID)
+stdfout=$( printf 'stdout/run%04d.out' $TF_TASKID)
+mkdir -p $SCRATCH/lilac_output
+./lilac $SCRATCH/$fout $TF_TASKID > $stdfout
