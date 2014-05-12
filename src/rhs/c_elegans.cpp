bb38f9d01a3db8e6060b238df8e829f9134cb191
Thundershield <schets@uw.edu>
2014-05-12 13:56:26 -0700

    Added some comments to c_elegans for easier reading

--- src/rhs/c_elegans.cpp
+++ src/rhs/c_elegans.cpp
@@ -15,11 +15,12 @@ int c_elegans::dxdt(ptr_passer x,  ptr_passer dx, double dt){
     double* restr dv = dx.get<double>();
     double* restr ds = dv+dim_v;
     double* restr s = v+dim_v;
+    //map eigen arrays over input pointers
     Map<Array<double, dim_v, 1>> vmap(v);
     Map<Array<double, dim_v, 1>> dvmap(dv);
     Map<Array<double, dim_v, 1>> smap(s);
     Map<Array<double, dim_v, 1>> dsmap(ds);
-
+    //calculations from matlab file
     sig = 1.0 / (1.0 + (-1*beta*(vmap-vmean)).exp());
     dsmap = ar*(sig * (1.0-smap)) - ad*smap;
     Iohm = (memG*(vmap - memV));
@@ -32,13 +33,15 @@ int c_elegans::dxdt(ptr_passer x,  ptr_passer dx, double dt){
             (gchem *(vmap*(AEchem_trans*smap.matrix()).array()
                      - (AEchem_trans*(smap*Echem).matrix()).array())));*/
     dvmap = (-1.0/tau)*(Iohm + Ielec + Ichem);
+    //current injection-for now is hard coded below, inj_nodes is empty here
     for(auto s : inj_nodes){
         dv[s] += (-1.0/tau)*cur_inj;
     }
     double amp = 2e4;
     dv[276]+= (-1.0/tau)*amp;
     dv[278]+= (-1.0/tau)*amp;
-    err("", "", "", FATAL_ERROR);
+    //This lines ends after one call to dxdt, this make it easy to check the output of one call
+    //err("", "", "", FATAL_ERROR);
     return 0;
 }
 int c_elegans::dwdt(ptr_passer x, ptr_passer _dx, double dt){
@@ -61,6 +64,11 @@ void read_mat(std::string fname, sp_t& in_mat){
     }
     in_mat.setFromTriplets(intr.begin(), intr.end());
 }
+/*!
+ * This function does the processing for the c_elegans class.
+ *
+ * It initializes the various matrices and reads values from the input files
+ */
 void c_elegans::postprocess(input& in){
     rhs_type::postprocess(in);
     if(dimension != num_neur*2){
@@ -85,23 +93,23 @@ void c_elegans::postprocess(input& in){
     laplacian.resize(num_neur, num_neur);
     read_mat(ag_fname, ag_m);
     read_mat(a_fname, a_m);
+    //spares matrices don't have columnwise operations, so temporary dense copies must be made
+    //for the initialization processes
     Matrix<double, Dynamic, Dynamic> ag_dense(num_neur, num_neur);
     ag_dense = ag_m *  Matrix<double, num_neur, num_neur>::Identity();
     auto sumvals = ag_dense.colwise().sum();
     sparse_type temp(num_neur, num_neur);
+    //generate the sparse diagonal matrix to build the lapacian
     std::vector<Triplet<double, int> > temp_tr;
     for(int i = 0; i < (int)num_neur; i++){
         temp_tr.push_back(Triplet<double, int>(i, i, sumvals[i]));
     }
     temp.setFromTriplets(temp_tr.begin(), temp_tr.end());
     laplacian = temp - ag_m;
+    //create transposed sparse matrix AEchem
     AEchem_trans.resize(num_neur, num_neur);
     AEchem_trans = a_m.transpose();
-    //will add rest later, this is just for performance test
-    for(size_t i = 0; i < num_neur; i++){
-        Echem[i] = EchemEx;
-        vmean[i] = EchemInh;
-    }
+    //initialize the Echem array
     for(size_t i = 0; i < num_neur; i++){
         if(GABAergic[i]){
             Echem[i] = EchemInh;
@@ -110,32 +118,37 @@ void c_elegans::postprocess(input& in){
             Echem[i] = EchemEx;
         }
     }
+    //Initialize the sig array
     for(size_t i = 0; i < num_neur; i++){
         sig[i] = 0.5;
     }
     
     eqS = sig * (ar/(ar*sig + ad));
+    //more initialization of temporary dense matrices
     Matrix<double, Dynamic, Dynamic> ldense(num_neur,num_neur);
     ldense = laplacian*Matrix<double, num_neur, num_neur>::Identity();
     Matrix<double, Dynamic, Dynamic> aedense(num_neur, num_neur);
     aedense= AEchem_trans*Matrix<double, num_neur, num_neur>::Identity();
     Matrix<double, Dynamic, Dynamic> C(num_neur, num_neur);
+    //create the C matrix
     C= memG*Matrix<double, num_neur, num_neur>::Identity() + gelec*ldense;
+    //initialize matrix to modify diagonal part of C
     Matrix<double, num_neur, 1> tmp =(gchem * aedense * eqS.matrix()); 
     for(size_t i = 0; i < num_neur; i++){
         C(i, i) += tmp(i);
     }
-    std::cout << C.colwise().sum() << std::endl;
     Matrix<double, num_neur, 1> Ivals;
     Ivals.setZero();
     double amp=2e4;
     Ivals[276]=amp;
     Ivals[278]=amp;
     Matrix<double, num_neur, 1> b;
+    //create B vector
     b= gchem*aedense*(eqS * Echem).matrix();
     for(size_t i = 0; i < num_neur; i++){
         b[i] += (memG*memV + Ivals[i]);
     }
+    //calculate eqV
     eqV.matrix() = C.inverse()*b;
     vmean = eqV+(1.0/beta) * (1.0/sig - 1).log();
 }
@@ -152,6 +165,7 @@ void c_elegans::initial_condition(ptr_passer in, size_t len){
         rhs_type<double>::initial_condition(in, len);
     }
     double* vals = in.get<double>();
+    //create fake initial condition
     for(size_t i = 0; i < num_neur; i++){
         vals[i] = eqV[i];
         vals[i+dim_v] = eqS[i];
