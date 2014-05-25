f9c366498775025737914848c9b92cadcf3b3482
Thundershield <schets@uw.edu>
2014-05-25 12:10:01 -0700

    Updated Documentation

--- src/writer/writer.cpp
+++ src/writer/writer.cpp
@@ -1,35 +1,47 @@
 #include "writer.h"
+
+/*!
+ * This functions adds the data to the list of values to be written,
+ * as long as the rule corresponding to the type is set to true
+ *
+ * @param inval The data to be saved
+ * @param dat_t The type of the input data
+ */
 void writer::add_data(std::shared_ptr<const data> inval, const w_type dat_t){
     if(will_write[dat_t]){
         dvals.push_back(inval);
     }
 }
-void writer::add_data(std::shared_ptr<const data> inval, const std::string dat_t){
-    add_data(inval, from_name(dat_t));
-}
-
+/*!
+ * This functions sets the rule for the input type
+ * @param inval The type which is having a rule reset
+ * @param rule the new rule for the type
+ */
 void writer::set_rule(w_type inval, bool rule){
     will_write[inval] = rule;
 }
-void writer::set_rule(std::string inval, bool rule){
-    set_rule(from_name_strict(inval), rule);
-}
 
+/*!
+ * This creates a new writer class with all of the type rules set to def
+ * @param def The default rule for all of the types
+ */
 writer::writer(bool def){
     for(int i = BEGIN; i <= END; i++){
         will_write[i] = def;
     }
 }
-writer::writer(const std::map<int, bool>& writevals, bool def){
-    //don't use initializer list since input needs to be sanitized 
-    for(const auto& val : writevals){
+
+/*!
+ * This constructs a writer that has the same rules as the map writevals, with any
+ * rule that isn't in writevals set to def
+ *
+ * @param writevals The current set of rules being used
+ * @param def The default rule for values not mapped by writevals
+ */
+writer::writer(const std::map<int, bool>& writevals, bool def):writer(def){
+    for(auto val : writevals){
         if(is_w_type(val.first)){
             will_write[val.first] = val.second;
-        }
-    }
-    for(int i = BEGIN; i < END; i++){
-        if(!will_write.count(i)){
-            will_write[i] = def;
         }
     }
 };
@@ -45,29 +57,7 @@ bool writer::is_w_type(int inval){
     return false;
 }
 
-writer::w_type writer::from_name(const std::string inval){
-    const static std::map<std::string, w_type> stvals = {{ "FINAL_SCORE", FINAL_SCORE},
-        {"FINAL_FUNC", FINAL_FUNC}, {"INTERMEDIATE_FUNC", INTERMEDIATE_FUNC},
-        {"INTERMEDIATE_SCORE", INTERMEDIATE_SCORE}, {"OTHER", OTHER}, {"INDEX", INDEX}};
-    if(!stvals.count(inval)){
-        err(std::string("Data type of ") + inval + std::string(" not found, check capitalization and spelling. Returning OTHER."),
-                "writer::from_name", "writer/writer.cpp", WARNING);
-        return OTHER;
-    }
-    return stvals.at(inval);
-}
-writer::w_type writer::from_name_strict(const std::string inval){
-    const static std::map<std::string, w_type> stvals = {{ "FINAL_SCORE", FINAL_SCORE},
-        {"FINAL_FUNC", FINAL_FUNC}, {"INTERMEDIATE_FUNC", INTERMEDIATE_FUNC},
-        {"INTERMEDIATE_SCORE", INTERMEDIATE_SCORE}, {"OTHER", OTHER}, {"INDEX", INDEX}};
-    if(!stvals.count(inval)){
-        err(std::string("Data type of ") + inval + std::string(" not found, check capitalization and spelling."),
-                "writer::from_name", "writer/writer.cpp", FATAL_ERROR);
-        //never reaches this point
-        return OTHER;
-    }
-    return stvals.at(inval);
-}
+
 void writer::write(std::ostream& ofile) const{
     for(const auto& val : dvals){
         ofile << val->to_string();
--- src/writer/writer.h
+++ src/writer/writer.h
@@ -9,36 +9,33 @@
 #include "utils/defs.hpp"
 #include "data.hpp"
 class writer{
+    //!Holds each data point
     std::list<std::shared_ptr<const data>> dvals;
+    //!Maps enum parameters to a bool deciding whether that label will write or not
     std::map<int, bool> will_write;
     friend class engineimp;
+    //!Writes the current data to a file
     virtual void write(std::ostream& ofile) const;
+    static bool is_w_type(int inval);
     public:
+    //!Labels for various input types
     typedef enum{
         FINAL_SCORE,
         FINAL_FUNC,
         INTERMEDIATE_FUNC,
         INTERMEDIATE_SCORE,
         INDEX,
+        PARAMETER,
         OTHER,
         BEGIN=FINAL_SCORE,
         END=OTHER} w_type;
 
     virtual void add_data(std::shared_ptr<const data> inval, const w_type dat_t);
-    virtual void add_data(std::shared_ptr<const data> inval, const std::string dat_t);
 
     void set_rule(w_type inval, bool rule);
-    void set_rule(std::string inval, bool rule);
 
     virtual ~writer();
-    writer(bool def = false);
-    writer(const std::map<int, bool>& writevals, bool def = false);
-    //won't implement copy for now since brings up question of multiple data
-    //writer(const writer& win);
-
-    static w_type from_name(const std::string inval);
-    //same as above except throws an error if bad name
-    static w_type from_name_strict(const std::string inval);
-    static bool is_w_type(int inval);
+    writer(bool def = true);
+    writer(const std::map<int, bool>& writevals, bool def = true);
 };
 #endif
