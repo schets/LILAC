7ebc7ca4e4ae7b7f416de7c7a30b34f81896a09d
Sam Schetterer <schets@uw.edu>
2014-06-18 12:30:13 -0700

    Improved nersc submission process

--- rundir/tf_script.sh
+++ rundir/tf_script.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 fout=$( printf 'lilac_output/run%04d.out' $TF_TASKID)
-stdout=$( printf 'output/lilac_stdout/run%04d.out' $TF_TASKID)
-mkdir -p $SCRATCH/lilac_output
-mkdir -p output
-mkdir -p output/lilac_stdout
-./lilac $SCRATCH/$fout $TF_TASKID 1>$stdout 2>$stdout
+stdout=$( printf 'output/run%04d.out' $TF_TASKID)
+stdout=$( printf 'output/run%04d.err' $TF_TASKID)
+./lilac+pat $SCRATCH/$fout $TF_TASKID 1>$stdout 2>$stdout
